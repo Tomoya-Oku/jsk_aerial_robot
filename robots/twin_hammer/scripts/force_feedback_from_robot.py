b2f400dc81b4cf2e390f164b65d9679bc810a19c
Kotaro Kaneko <k.koutarou.0623@gmail.com>
2025-04-02 00:50:36 +0900

    [WIP] [Twin Hammer] [force feedback] create exp and log converter for force feedback from ftsensor value something wrong on log converter

---
+++ robots/twin_hammer/scripts/force_feedback_from_robot.py
@@ -0,0 +1,106 @@
+#!/usr/bin/env python
+
+import rospy
+import time
+import math
+import tf.transformations as tf
+from scipy.optimize import fsolve
+from std_msgs.msg import Int8
+from aerial_robot_msgs.msg import FlightNav
+from spinal.msg import DesireCoord
+from geometry_msgs.msg import PoseStamped, WrenchStamped
+
+def exponential(x, base, k_exp):
+  # return math.pow(base,x) * k_exp
+  return pow(x,base) * k_exp
+
+def exp_eqs(vars, base):
+  x, k_exp = vars
+  eq1 = exponential(x,base,k_exp) - x
+  # eq2 = math.log(base) * exponential(x,base,k_exp) - 1
+  eq2 = base * exponential(x,base-1,k_exp) - 1
+  return [eq1,eq2]
+
+def find_range_k_exp(base, initial_guess=(1.0,1.0)):
+  range_sol, k_exp_sol = fsolve(exp_eqs, initial_guess, args=(base))
+  return range_sol, k_exp_sol
+
+def logarithm(x, base, k_log):
+  return math.log(x,base) * k_log
+
+def log_eqs(vars, base):
+  x,k_log = vars
+  eq1 = logarithm(x,base,k_log) - x
+  eq2 = k_log / (math.log(base)*x) - 1
+  return [eq1,eq2]
+
+def find_range_k_log(base, initial_guess=(1.0,1.0)):
+  range_sol, k_log_sol = fsolve(log_eqs, initial_guess, args=(base))
+  return range_sol, k_log_sol
+
+class force_feedback_from_robot():
+
+  def __init__(self):
+
+    self.real_machine = rospy.get_param("~real_machine", False)
+    self.robot_name = rospy.get_param("~robot_name", "quadrotor")
+    self.pos_scale = rospy.get_param("~pos_scale", 1.1)
+
+    # self.haptics_switch_pub = rospy.Publisher('/twin_hammer/haptics_switch', Int8, queue_size=1)
+    self.haptics_wrench_pub = rospy.Publisher('/twin_hammer/haptics_wrench', WrenchStamped, queue_size=1)
+    self.robot_wrench_sub = rospy.Subscriber('/filtered_ft_sensor', WrenchStamped, self.robot_wrench_cb)
+
+    self.haptics_wrench_msg = WrenchStamped()
+    self.robot_wrench = []
+
+    self.k_p = 1.0
+    self.exp_base = 1.3
+    self.log_base = 2.5
+    self.range_exp, self.k_exp = find_range_k_exp(self.exp_base)
+    self.range_log, self.k_log = find_range_k_log(self.log_base)
+    # time.sleep(0.5)
+
+  def robot_wrench_cb(self,msg):
+    self.robot_wrench = [msg.wrench.force.x, msg.wrench.force.y, msg.wrench.force.z, msg.wrench.torque.x, msg.wrench.torque.y, msg.wrench.torque.z]
+
+  def main(self):
+    r = rospy.Rate(40)
+    while not rospy.is_shutdown():
+      haptics_wrench = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
+
+      for i in range(len(self.robot_wrench)):
+        wrench_i = self.robot_wrench[i]
+
+        # propotional conversion
+        haptics_wrench[i] = wrench_i * self.k_p
+
+        # exponential conversion
+        if wrench_i>self.range_exp:
+          haptics_wrench[i] = exponential(wrench_i, self.exp_base, self.k_exp)
+        elif wrench_i<-self.range_exp:
+          haptics_wrench[i] = -exponential(-wrench_i, self.exp_base, self.k_exp)
+        else:
+          haptics_wrench[i] = wrench_i
+
+        # log conversion
+        if wrench_i>self.range_log:
+          haptics_wrench[i] = logarithm(wrench_i, self.log_base, self.k_log)
+        elif wrench_i<-self.range_log:
+          haptics_wrench[i] = -logarithm(-wrench_i, self.log_base, self.k_log)
+        else:
+          haptics_wrench[i] = wrench_i
+
+        self.haptics_wrench_msg.wrench.force.x = haptics_wrench[0]
+        self.haptics_wrench_msg.wrench.force.y = haptics_wrench[1]
+        self.haptics_wrench_msg.wrench.force.z = haptics_wrench[2]
+        self.haptics_wrench_msg.wrench.torque.x = haptics_wrench[3]
+        self.haptics_wrench_msg.wrench.torque.y = haptics_wrench[4]
+        self.haptics_wrench_msg.wrench.torque.z = haptics_wrench[5]
+        self.haptics_wrench_pub.publish(self.haptics_wrench_msg)
+
+      r.sleep()
+
+if __name__ == "__main__":
+  rospy.init_node("force_feedback_from_robot")
+  Tracker = force_feedback_from_robot()
+  Tracker.main()
---
+++ robots/twin_hammer/scripts/func_test_force_feedback.py
@@ -0,0 +1,81 @@
+#!/usr/env python
+import numpy as np
+import matplotlib.pyplot as plt
+import math
+from scipy.optimize import fsolve
+
+def exponential(x, base, k_exp):
+  # return math.pow(base,x) * k_exp
+  return pow(x,base) * k_exp
+
+def exp_eqs(vars, base):
+  x, k_exp = vars
+  eq1 = exponential(x,base,k_exp) - x
+  # eq2 = math.log(base) * exponential(x,base,k_exp) - 1
+  eq2 = base * exponential(x,base-1,k_exp) - 1
+  return [eq1,eq2]
+
+def find_range_k_exp(base, initial_guess=(1.0,1.0)):
+  range_sol, k_exp_sol = fsolve(exp_eqs, initial_guess, args=(base))
+  return range_sol, k_exp_sol
+
+def logarithm(x, base, k_log):
+  return math.log(x,base) * k_log
+
+def log_eqs(vars, base):
+  x,k_log = vars
+  eq1 = logarithm(x,base,k_log) - x
+  eq2 = (k_log / (x*math.log(base, math.e))) - 1
+  return [eq1,eq2]
+
+def find_range_k_log(base, initial_guess=(1.0,1.0)):
+  range_sol, k_log_sol = fsolve(log_eqs, initial_guess, args=(base))
+  return range_sol, k_log_sol
+
+exp_base = 1.2
+log_base = 1.5
+y_exp_list = []
+y_log_list = []
+x = np.linspace(-10, 10, 1000)
+range_exp, k_exp = find_range_k_exp(exp_base)
+print(range_exp)
+range_log, k_log = find_range_k_log(log_base)
+print(range_log)
+for i in x:
+  if i>range_exp:
+    y_exp = exponential(i,exp_base,k_exp)
+  elif i<-range_exp:
+    y_exp = -exponential(-i,exp_base,k_exp)
+  else:
+    y_exp = i
+  y_exp_list.append(y_exp)
+for i in x:
+  if i>range_log:
+    y_log = logarithm(i,log_base,k_log)
+  elif i<-range_log:
+    y_log = -logarithm(-i,log_base,k_log)
+  else:
+    y_log = i
+  y_log_list.append(y_log)
+
+plt.figure(figsize=(8, 6))
+plt.plot(x, y_exp_list, label="exp", color='orange')
+plt.axvline(-range_exp, color='gray', linestyle='--', alpha=0.7)
+plt.axvline(range_exp, color='gray', linestyle='--', alpha=0.7)
+plt.axhline(0, color='black', linewidth=0.5, alpha=0.7)
+plt.xlabel("x")
+plt.ylabel("y")
+plt.legend()
+plt.grid()
+plt.show()
+
+plt.figure(figsize=(8, 6))
+plt.plot(x, y_log_list, label="log", color='orange')
+plt.axvline(-range_log, color='gray', linestyle='--', alpha=0.7)
+plt.axvline(range_log, color='gray', linestyle='--', alpha=0.7)
+plt.axhline(0, color='black', linewidth=0.5, alpha=0.7)
+plt.xlabel("x")
+plt.ylabel("y")
+plt.legend()
+plt.grid()
+plt.show()
