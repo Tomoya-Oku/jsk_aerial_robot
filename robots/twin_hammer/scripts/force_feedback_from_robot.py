1fb3d17dccc5da7d90f5194382c572bb835123d8
Kotaro Kaneko <k.koutarou.0623@gmail.com>
2025-04-02 23:45:54 +0900

    [WIP] [Twin Hammer] [force feedback] debug of log converter make k_log and log_base independently

--- robots/twin_hammer/scripts/force_feedback_from_robot.py
+++ robots/twin_hammer/scripts/force_feedback_from_robot.py
@@ -4,39 +4,16 @@ import rospy
 import time
 import math
 import tf.transformations as tf
-from scipy.optimize import fsolve
 from std_msgs.msg import Int8
 from aerial_robot_msgs.msg import FlightNav
 from spinal.msg import DesireCoord
 from geometry_msgs.msg import PoseStamped, WrenchStamped
 
 def exponential(x, base, k_exp):
-  # return math.pow(base,x) * k_exp
   return pow(x,base) * k_exp
-
-def exp_eqs(vars, base):
-  x, k_exp = vars
-  eq1 = exponential(x,base,k_exp) - x
-  # eq2 = math.log(base) * exponential(x,base,k_exp) - 1
-  eq2 = base * exponential(x,base-1,k_exp) - 1
-  return [eq1,eq2]
-
-def find_range_k_exp(base, initial_guess=(1.0,1.0)):
-  range_sol, k_exp_sol = fsolve(exp_eqs, initial_guess, args=(base))
-  return range_sol, k_exp_sol
 
 def logarithm(x, base, k_log):
   return math.log(x,base) * k_log
-
-def log_eqs(vars, base):
-  x,k_log = vars
-  eq1 = logarithm(x,base,k_log) - x
-  eq2 = k_log / (math.log(base)*x) - 1
-  return [eq1,eq2]
-
-def find_range_k_log(base, initial_guess=(1.0,1.0)):
-  range_sol, k_log_sol = fsolve(log_eqs, initial_guess, args=(base))
-  return range_sol, k_log_sol
 
 class force_feedback_from_robot():
 
@@ -55,9 +32,11 @@ class force_feedback_from_robot():
 
     self.k_p = 1.0
     self.exp_base = 1.3
-    self.log_base = 2.5
-    self.range_exp, self.k_exp = find_range_k_exp(self.exp_base)
-    self.range_log, self.k_log = find_range_k_log(self.log_base)
+    self.log_base = 1.3
+    self.k_exp = 1.0
+    self.k_log = 1.1
+    self.range_log = math.e
+    self.a_log = self.k_log / (math.e * math.log(self.log_base))
     # time.sleep(0.5)
 
   def robot_wrench_cb(self,msg):
@@ -75,12 +54,10 @@ class force_feedback_from_robot():
         haptics_wrench[i] = wrench_i * self.k_p
 
         # exponential conversion
-        if wrench_i>self.range_exp:
+        if wrench_i>=0.0:
           haptics_wrench[i] = exponential(wrench_i, self.exp_base, self.k_exp)
-        elif wrench_i<-self.range_exp:
-          haptics_wrench[i] = -exponential(-wrench_i, self.exp_base, self.k_exp)
         else:
-          haptics_wrench[i] = wrench_i
+          haptics_wrench[i] = -exponential(-wrench_i, self.exp_base, self.k_exp)
 
         # log conversion
         if wrench_i>self.range_log:
@@ -88,7 +65,7 @@ class force_feedback_from_robot():
         elif wrench_i<-self.range_log:
           haptics_wrench[i] = -logarithm(-wrench_i, self.log_base, self.k_log)
         else:
-          haptics_wrench[i] = wrench_i
+          haptics_wrench[i] = wrench_i * self.a_log
 
         self.haptics_wrench_msg.wrench.force.x = haptics_wrench[0]
         self.haptics_wrench_msg.wrench.force.y = haptics_wrench[1]
--- robots/twin_hammer/scripts/func_test_force_feedback.py
+++ robots/twin_hammer/scripts/func_test_force_feedback.py
@@ -20,34 +20,48 @@ def find_range_k_exp(base, initial_guess=(1.0,1.0)):
   return range_sol, k_exp_sol
 
 def logarithm(x, base, k_log):
+  # if x >= 0.0:
+  #   return math.log(x,base) * k_log
+  # else:
+  #   return math.log(-x,base) * k_log
   return math.log(x,base) * k_log
 
 def log_eqs(vars, base):
   x,k_log = vars
   eq1 = logarithm(x,base,k_log) - x
-  eq2 = (k_log / (x*math.log(base, math.e))) - 1
+  eq2 = (k_log / (x*math.log(base))) - 1
   return [eq1,eq2]
 
 def find_range_k_log(base, initial_guess=(1.0,1.0)):
   range_sol, k_log_sol = fsolve(log_eqs, initial_guess, args=(base))
   return range_sol, k_log_sol
 
-exp_base = 1.2
-log_base = 1.5
+exp_base = 1.3
+log_base = 1.3
+k_exp = 1.0
+k_log = 1.1
+a_log = k_log / (math.e*math.log(log_base))
+range_log = math.e
 y_exp_list = []
 y_log_list = []
 x = np.linspace(-10, 10, 1000)
-range_exp, k_exp = find_range_k_exp(exp_base)
-print(range_exp)
-range_log, k_log = find_range_k_log(log_base)
-print(range_log)
+# range_exp, k_exp = find_range_k_exp(exp_base)
+# print("range_exp = ",range_exp)
+# print("k_exp = ", k_exp)
+# range_log, k_log = find_range_k_log(log_base)
+# print("range_log = ", range_log)
+# print("k_log = ", k_log)
 for i in x:
-  if i>range_exp:
-    y_exp = exponential(i,exp_base,k_exp)
-  elif i<-range_exp:
-    y_exp = -exponential(-i,exp_base,k_exp)
-  else:
-    y_exp = i
+  if i >= 0:
+    y_exp = exponential(i, exp_base, k_exp)
+  if i < 0:
+    y_exp = -exponential(-i, exp_base, k_exp)
+  # if i>range_exp:
+  #   y_exp = exponential(i,exp_base,k_exp)
+  # elif i<-range_exp:
+  #   y_exp = -exponential(-i,exp_base,k_exp)
+  # else:
+  #   y_exp = i
   y_exp_list.append(y_exp)
 for i in x:
   if i>range_log:
@@ -55,13 +69,13 @@ for i in x:
   elif i<-range_log:
     y_log = -logarithm(-i,log_base,k_log)
   else:
-    y_log = i
+    y_log = i * a_log
   y_log_list.append(y_log)
 
 plt.figure(figsize=(8, 6))
 plt.plot(x, y_exp_list, label="exp", color='orange')
-plt.axvline(-range_exp, color='gray', linestyle='--', alpha=0.7)
-plt.axvline(range_exp, color='gray', linestyle='--', alpha=0.7)
+# plt.axvline(-range_exp, color='gray', linestyle='--', alpha=0.7)
+# plt.axvline(range_exp, color='gray', linestyle='--', alpha=0.7)
 plt.axhline(0, color='black', linewidth=0.5, alpha=0.7)
 plt.xlabel("x")
 plt.ylabel("y")
